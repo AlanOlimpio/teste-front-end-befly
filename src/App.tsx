d22f971ea2f0c6c8d6764eb9f8d9bc453e846c4e
Alan Olimpio <alancleyton.olimpio@gmail.com>
2025-07-22 13:57:47 -0300

    feat: adiciona @tanstack/react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import "./global.css";
 import { RouterProvider } from "react-router-dom";
-
 import { router } from "@/routes";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/react-query";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="teste-front-end">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
